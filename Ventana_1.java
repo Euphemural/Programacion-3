14587f809b5ebe34b4b13b431d12dd3e088f77b4
Uriel <Uriel>
2024-02-26 10:55:14 -0700

    Inicio de la calculadora

--- Ventana_1.java
+++ Ventana_1.java
@@ -16,7 +16,7 @@ public class Ventana_1 extends JFrame
         this.setVisible(true);
         this.setSize(1000, 600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("Mi ventana");
+        this.setTitle("Calculadora");
         this.setMinimumSize(new Dimension(250,250));
         this.setMaximumSize(new Dimension(1000,750));
         this.setResizable(true);
@@ -26,94 +26,32 @@ public class Ventana_1 extends JFrame
         this.iniciarComponentes();
     }
 	
-	public void iniciarComponentes() 
-	{
-		
-		JPanel login = new JPanel();
-		login.setSize(this.getWidth()/2, this.getHeight());
-		login.setBackground(Color.gray);
-		login.setLayout(null);
-		
-		JLabel login_tag = new JLabel("Acceder");
-		login_tag.setSize(310, 80);
-		login_tag.setLocation(85, 20);
-		login_tag.setOpaque(true);
-		login_tag.setBackground(Color.red);
-        login_tag.setHorizontalAlignment(SwingConstants.CENTER);
-        login_tag.setFont(new Font("Birds of Paradise", Font.ITALIC, 45));
-        login_tag.setForeground(Color.WHITE);
-		login.add(login_tag);
+    public void iniciarComponentes() {
+        
+        this.fondoCalculadora();
 
-        JLabel usuario = new JLabel("Usuario");
-        usuario.setSize(150, 30);
-        usuario.setLocation(20, 120);
-        usuario.setOpaque(true);
-        usuario.setBackground(Color.gray);
-        usuario.setFont(new Font("Brids of Paradise", Font.ITALIC + Font.BOLD, 18));
-        login.add(usuario);
+    }
 
-        JTextField user = new JTextField();
-        user.setSize(100, 30);
-        user.setLocation(20, 150);
-        user.setOpaque(true);
-        user.setBackground(Color.WHITE);
-        login.add(user);
+    public void fondoCalculadora()
+    {
+        JPanel fondoCalculadora = new JPanel();
+        fondoCalculadora.setSize(this.getWidth()/2, this.getHeight());
+        fondoCalculadora.setBackground(Color.BLACK);
+        fondoCalculadora.setLayout(null);
 
-        JLabel contraseña = new JLabel("contraseña");
-        contraseña.setSize(150, 30);
-        contraseña.setLocation(20, 250);
-        contraseña.setOpaque(true);
-        contraseña.setBackground(Color.gray);
-        contraseña.setFont(new Font("Brids of Paradise", Font.ITALIC + Font.BOLD, 18));
-        login.add(contraseña);
+        JButton boton_suma = new JButton("+");
+        boton_suma.setBounds(10, 50, 50, 30);
+        boton_suma.setOpaque(true);
+        boton_suma.setBackground(Color.WHITE);
+        boton_suma.setForeground(Color.BLACK);
+        boton_suma.setHorizontalTextPosition(SwingConstants.CENTER);
+        fondoCalculadora.add(boton_suma);
 
-        JPasswordField password = new JPasswordField();
-        password.setSize(100, 30);
-        password.setLocation(20, 290);
-        password.setOpaque(true);
-        password.setBackground(Color.WHITE);
-        login.add(password);
+        this.add(fondoCalculadora);
 
-        JButton recordar = new JButton();
-        recordar.setSize(20,20);
-        recordar.setLocation(20, 400);
-        recordar.setOpaque(true);
-        recordar.setBackground(Color.white);
-        login.add(recordar);
+    }
+    
 
-        JLabel recordar_usuario = new JLabel("Recordarme");
-        recordar_usuario.setSize(150, 30);
-        recordar_usuario.setLocation(50, 395);
-        recordar_usuario.setOpaque(true);
-        recordar_usuario.setBackground(Color.gray);
-        recordar_usuario.setFont(new Font("Brids of Paradise", Font.ITALIC + Font.BOLD, 14));
-        login.add(recordar_usuario);
-
-        JLabel contraseña_olvidada = new JLabel("¿Olvidastes tu contraseña?");
-        contraseña_olvidada.setSize(300, 30);
-        contraseña_olvidada.setLocation(285, 395);
-        contraseña_olvidada.setOpaque(true);
-        contraseña_olvidada.setBackground(Color.gray);
-        contraseña_olvidada.setFont(new Font("Brids of Paradise", Font.ITALIC + Font.BOLD, 14));
-        login.add(contraseña_olvidada);
-
-        JButton ingresar = new JButton("Ingresar");
-        ingresar.setSize(150, 50);
-        ingresar.setLocation(20,500);
-        ingresar.setOpaque(true);
-        ingresar.setBackground(Color.WHITE);
-        ingresar.setFont(new Font("Brids of Paradise", Font.ITALIC + Font.BOLD, 18));
-        login.add(ingresar);
-
-
-		JPanel registro = new JPanel();
-		registro.setSize(this.getWidth()/2, this.getHeight());
-		registro.setBackground(Color.green);
-        registro.setLayout(null);
-
-		this.add(login);
-		this.add(registro);
-	}
 
     public static void main(String[] args) {
         new Ventana_1();
